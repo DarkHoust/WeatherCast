b1241158b3ea723a8ba291f37e8575a6c4e01209
DarkHost <sultanisk@mail.ru>
2024-02-09 10:06:30 +0600

    Map issue fix

--- public/scripts/mainPage.js
+++ public/scripts/mainPage.js
@@ -1,3 +1,37 @@
+
+//Google Map API
+let map;
+
+async function initMap() {
+    try {
+        const locationParam = window.location.search.split('location=')[1];
+        const location = locationParam ? encodeURIComponent(locationParam) : 'Astana';
+
+
+        const response = await fetch(`http://localhost:3000/dataAPI?location=${location}`);
+        if (!response.ok) {
+            throw new Error(`Error fetching data: ${response.statusText}`);
+        }
+        const responseData = await response.json();
+
+
+        var lat = responseData.coordinates.lat;
+        var lon = responseData.coordinates.lon;
+
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: lat, lng: lon },
+            zoom: 10,
+            disableDefaultUI: true
+        });
+    } catch (error) {
+        console.error('Error fetching JSON:', error);
+    }
+}
+
+
+window.initMap = initMap;
+
+// On-Click handle functions
 function showMap() {
     let weatherView = document.querySelectorAll(".weather");
     let mapView = document.querySelectorAll(".map");
@@ -48,6 +82,7 @@ function showWeather() {
 
 function redirectToMainPage() {
     window.location.href = 'http://localhost:3000';
+    initMap();
 }
 
 function reloadPage(){
@@ -81,35 +116,3 @@ function logOut() {
     window.location.replace('/auth')
 }
 
-
-//Google Map API
-let map;
-
-async function initMap() {
-    try {
-        const locationParam = window.location.search.split('location=')[1];
-        const location = locationParam ? encodeURIComponent(locationParam) : 'Astana';
-
-
-        const response = await fetch(`http://localhost:3000/dataAPI?location=${location}`);
-        if (!response.ok) {
-            throw new Error(`Error fetching data: ${response.statusText}`);
-        }
-        const responseData = await response.json();
-
-
-        var lat = responseData.coordinates.lat;
-        var lon = responseData.coordinates.lon;
-
-        map = new google.maps.Map(document.getElementById("map"), {
-            center: { lat: lat, lng: lon },
-            zoom: 10,
-            disableDefaultUI: true
-        });
-    } catch (error) {
-        console.error('Error fetching JSON:', error);
-    }
-}
-
-
-window.initMap = initMap;
